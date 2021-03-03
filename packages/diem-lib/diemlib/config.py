e5ebe5c32b7eada33e2e163e8a04e1791fe57c58
guy-huinen <guy_huinen@be.ibm.com>
2021-03-03 12:33:06 +0100

    chore: adding python lib

---
+++ packages/diem-lib/diemlib/config.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# diemlib/config.py
+
+"""
+   This module contains general variables that can be used
+   througout the complete application
+"""
+
+__count = 0
+__email = None
+__filepath = None
+__id = None
+__jobid = None
+__jobstart: None
+__name = None
+__org: None
+__starttime: None
+__transid = None
+__url = None
+__isservice = False
