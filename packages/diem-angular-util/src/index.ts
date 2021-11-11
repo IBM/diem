31fefe89ad87ff2f8b8aba83fce687b42f7e92d9
guy-huinen <guy_huinen@be.ibm.com>
2021-11-11 20:38:10 +0100

    chore: fixing backtotop

--- packages/diem-angular-util/src/index.ts
+++ packages/diem-angular-util/src/index.ts
@@ -4,3 +4,5 @@ export { HttpService, IHttpError } from './http.service';
 export { Directives } from './directives';
 export { AppPages } from './pages';
 export { AppErrorComponent, ForbiddenComponent, NotFoundComponent, UnauthorizedComponent } from './pages';
+export { IBMBackToTopDirective } from './ibm-back-to-top.directive';
+export { IBMStickyHeaderDirective } from './ibm.sticky.header.directive';
