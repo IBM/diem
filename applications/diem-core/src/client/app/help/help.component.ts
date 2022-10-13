bf5f1ad1e66374e15baf6dc3aec0738122e9b6d7
guy-huinen <guy_huinen@be.ibm.com>
2022-10-13 14:14:14 +0200

    chore: update packages, help and terms
    Signed-off-by: guy-huinen <guy_huinen@be.ibm.com>

--- applications/diem-core/src/client/app/help/help.component.ts
+++ applications/diem-core/src/client/app/help/help.component.ts
@@ -1,7 +1,49 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ChangeDetectorRef } from '@angular/core';
+import { HttpService } from '@mydiem/diem-angular-util';
+import { appConfig } from '../../app.config';
 import { tmpl } from './templates/help.pug.tmpl';
 
 @Component({
+    changeDetection: ChangeDetectionStrategy.OnPush,
     template: tmpl,
 })
-export class HelpComponent {}
+export class HelpComponent {
+    public content?: string;
+
+    private contentLoaded = false;
+    private httpService: HttpService;
+    private cd: ChangeDetectorRef;
+
+    public constructor(httpService: HttpService, cd: ChangeDetectorRef) {
+        this.httpService = httpService;
+        this.cd = cd;
+
+        if (!this.contentLoaded) {
+            void this.updateSite();
+        }
+    }
+
+    private updateSite(): void {
+        const url = `${appConfig.formsurl}help`;
+
+        this.httpService.httpGet(url).subscribe({
+            next: (response: any) => {
+                console.info('$terms (updateSite): adding value....');
+                this.content = response.content;
+                this.contentLoaded = true;
+                this.check('content loaded');
+            },
+            error: (error: unknown) => {
+                if (typeof error === 'object') {
+                    const err = error as Error;
+                    console.warn('$terms (updateSite): error', err);
+                }
+            },
+        });
+    }
+
+    private check = (from: string) => {
+        console.info(`%c$sterms (check): called by => ${from}`, 'color:red');
+        this.cd.markForCheck();
+    };
+}
